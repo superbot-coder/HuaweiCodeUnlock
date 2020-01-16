1e3e5eef697911c4249ed63719900332fb52a17b
superbot-coder <knst.spb@gmail.com>
2020-01-16 23:03:59 +0800

    Update HuaweiCodeUnlock.pas

--- HuaweiCodeUnlock.pas
+++ HuaweiCodeUnlock.pas
@@ -3,7 +3,7 @@ unit HuaweiCodeUnlock;
 //****************************************************
 //  HUAWEI CODE CALCULATOR "ALGO V1+V2+V3/201"
 //  Autor by SUPERBOT
-//  Conver from code PHP and Python modules
+//  Convert from code PHP and Python modules
 //  Requirement for compiling modul: DCPcrypt2-2010
 //  Source PHP: https://github.com/yanderemoe/huawei_modem_code_calculator
 //  Source Python: https://gist.github.com/DonnchaC/09c9de3a73b0fd29c699d4f3ce038074
